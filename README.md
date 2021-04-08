343d3ce3d91b6e50b140944f13c156c88494ec2a
Peter Yangs <763492711@qq.com>
2021-04-08 21:06:37 +0800

    comment for bro

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # star-rain
 星星雨项目开发
+
+## Build Setup
+
+``` bash
+#可以百度一下如何配置淘宝镜像，配置之后下面的npm都可以替换为cnpm
+#示例是components文件夹的static_example文件，可以用那个先做出一个自己的table出来放在一个新路由中。
+#下载下来的话要先安装对应的安装环境，包括node，npm，git这些，有问题可以直接问我
+
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/static_example.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="table">
+    <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="type"
+      label="类别"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="grade"
+      label="分数">
+    </el-table-column>
+  </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Form',
+  data () {
+    return {
+      tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            grade: '4分'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            grade: '4分'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            grade: '4分'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            grade: '4分'
+          }]
+    }
+  },
+  methods: {
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
